ba57512434abc1a7c6bb7eba666e95b2d06cdf95
lucianovianna <luciano11v11@gmail.com>
2020-02-02 17:45:48 -0300

    Working on layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,13 @@ class App extends StatelessWidget {
         fontFamily: 'Roboto',
         textTheme: TextTheme(
           title: TextStyle(
-            fontSize: 56.0,
+            fontSize: 36.0,
             fontWeight: FontWeight.w700,
             color: Colors.black,
           ),
           display1: TextStyle(
             fontSize: 23.0,
-            fontWeight: FontWeight.w700,
+            fontWeight: FontWeight.w500,
             color: Colors.black87,
           ),
         ),
@@ -36,7 +36,7 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Expense Tracker"),
       ),
-      body: Column(
+      body: ListView(
         children: [DglSection()],
       ),
     );
@@ -49,42 +49,60 @@ class DglSection extends StatelessWidget {
     var display1 = Theme.of(context).textTheme.display1;
     var title = Theme.of(context).textTheme.title;
 
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 26, horizontal: 14),
-      child: Row(
+    double expensedMoney = 500.0;
+    double gainedMoney = 1000.0;
+    double profitedMoney = gainedMoney - expensedMoney;
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-              padding: const EdgeInsets.only(bottom: 32),
-              child: Center(
-                child: Text(
-                  "This month",
-                  style: title,
-                ),
-              )),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: Text(
-                    "Expenses",
-                    style: display1,
-                  ),
-                ),
-                Text(
-                  "Gains",
-                  style: display1,
-                ),
-                Text(
-                  "Profit",
-                  style: display1,
-                ),
-              ],
+            padding: const EdgeInsets.symmetric(vertical: 26.0),
+            child: Text(
+              "This month",
+              style: title,
             ),
-          )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildDglColumns(["Expenses", "Gains", "Profit"], display1),
+              _buildDglColumns(
+                  ["\$ $expensedMoney", "\$ $gainedMoney", "\$ $profitedMoney"],
+                  display1),
+            ],
+          ),
         ],
       ),
+    );
+  }
+
+  Column _buildDglColumns(List<String> label, var display1) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Text(
+            label[0],
+            style: display1,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Text(
+            label[1],
+            style: display1,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Text(
+            label[2],
+            style: display1,
+          ),
+        ),
+      ],
     );
   }
 }
