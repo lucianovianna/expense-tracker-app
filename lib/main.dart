5032d4bdf9ab0f65ebc628240ba079463ea74344
lucianovianna <luciano11v11@gmail.com>
2020-02-13 17:28:21 -0300

    Working on Last Inputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,35 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:states_rebuilder/states_rebuilder.dart';
+import 'package:states_rebuilder/states_rebuilder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'models/Entry.dart';
 
 void main() => runApp(App());
+
+class Data extends StatesRebuilder {
+  var entries = new List<Entry>();
+
+  Data() {
+    entries = [];
+
+    entries.add(Entry(category: "Job", isExpense: false, value: 500.0));
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Entry> result = decoded.map((i) => Entry.fromJson(i)).toList();
+
+      entries = result;
+
+      rebuildStates();
+    }
+  }
+}
 
 class App extends StatelessWidget {
   @override
@@ -26,7 +53,12 @@ class App extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: Injector(
+        inject: [Inject(() => Data())],
+        builder: (BuildContext context) {
+          return MyHomePage();
+        },
+      ),
     );
   }
 }
@@ -38,9 +70,9 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Expense Tracker"),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: ListView(
+        // mainAxisSize: MainAxisSize.max,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           DglSection(),
           LastInputsSection(),
@@ -120,30 +152,48 @@ class DglSection extends StatelessWidget {
 class LastInputsSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var title = Theme.of(context).textTheme.title;
+    var display1 = Theme.of(context).textTheme.display1;
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.only(top: 32.0),
-          child: Text("Last Inputs", style: title),
-        ),
-        ListView(
-          padding: const EdgeInsets.symmetric(vertical: 4.0),
-          children: <Widget>[
-            ListTile(
-              title: Text("Expense One"),
-              subtitle: Text("Category"),
+    final Data dataModel = Injector.get(context: context);
+
+    return ListView.builder(
+      itemCount: dataModel.entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        dataModel.load();
+        final entry = dataModel.entries[index];
+
+        dynamic isExpense() {
+          String entryType = "Gain";
+
+          if (entry.isExpense) {
+            entryType = "Expense";
+          }
+
+          return ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Text>[
+                Text(
+                  "$entryType $index",
+                  style: display1,
+                  textAlign: TextAlign.right,
+                ),
+                Text(
+                  "R\$ $entry.value",
+                  style: display1,
+                  textAlign: TextAlign.left,
+                ),
+              ],
             ),
-            ListTile(
-              title: Text("Gain One"),
-              subtitle: Text("Category"),
-            ),
-          ],
-        )
-      ],
+            subtitle: Text("$entry.category", style: display1),
+          );
+        }
+
+        return Container(
+          child: isExpense(),
+          key: Key(dataModel.entries.indexOf(entry).toString()),
+        );
+      },
     );
   }
 }
