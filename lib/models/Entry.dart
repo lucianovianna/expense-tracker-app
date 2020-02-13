52e63e72954fbaf2b7bf3beed35f6f1a6dea553c
lucianovianna <luciano11v11@gmail.com>
2020-02-13 15:36:35 -0300

    Update Model

--- lib/models/Entry.dart
+++ lib/models/Entry.dart
@@ -1,19 +1,19 @@
 class Entry {
-  String type;
+  bool isExpense;
   double value;
   String category;
 
-  Entry({this.type, this.value, this.category});
+  Entry({this.isExpense, this.value, this.category});
 
   Entry.fromJson(Map<String, dynamic> json) {
-    type = json['type'];
+    isExpense = json['isExpense'];
     value = json['value'];
     category = json['category'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['type'] = this.type;
+    data['isExpense'] = this.isExpense;
     data['value'] = this.value;
     data['category'] = this.category;
     return data;
